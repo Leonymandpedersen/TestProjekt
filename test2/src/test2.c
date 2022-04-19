d0e3b574f6e36c2af0c633fda2548ab4bd4ec505
leo <leo@leo-VirtualBox>
2022-04-19 16:55:54 +0200

    test

--- test2/src/test2.c
+++ test2/src/test2.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : test2.c
- Author      : 1
+ Author      :
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
